91986f9a3ac8e4d46c88391376b31af62fdd248c
rsamec <roman.samec2@gmail.com>
2015-06-20 08:19:41 +0200

    Update 2015-06-20T06:19:37.667Z

--- src/Chart.js
+++
@@ -1,6 +0,0 @@
-import Pie from './charts/Pie.js';
-import Tree from './charts/Tree.js';
-import SmoothLine from './charts/SmoothLine.js';
-
-
-export default {Pie:Pie,Tree:Tree,SmoothLine:SmoothLine};
