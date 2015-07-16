f34da3b56e41b43e029dec877f3d09a36f57d684
rsamec <roman.samec2@gmail.com>
2015-07-16 19:31:04 +0200

    vivus - deprecated

--- src/Chart.js
+++ src/Chart.js
@@ -3,7 +3,14 @@ import Tree from './charts/Tree.js';
 import Radar from './charts/Radar.js';
 import Bar from './charts/Bar.js';
 import SmoothLine from './charts/SmoothLine.js';
-import SmoothLineVivus from './charts/SmoothLineVivus.js';
+//import SmoothLineVivus from './charts/SmoothLineVivus.js';
 import Scatterplot from './charts/Scatterplot.js';
 
-export default {Pie:Pie,Tree:Tree,SmoothLine:SmoothLine, SmoothLineVivus:SmoothLineVivus,Radar:Radar,Bar:Bar,Scatterplot:Scatterplot};
+export default {
+    Pie:Pie,
+    Tree:Tree,
+    SmoothLine:SmoothLine,
+    SmoothLineVivus:SmoothLine,
+    Radar:Radar,
+    Bar:Bar,
+    Scatterplot:Scatterplot};
